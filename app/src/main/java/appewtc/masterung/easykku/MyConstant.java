addef4d1c7c606e1ea6bae05f53352a374025c2b
masterUNG <phrombutr@gmail.com>
2016-11-13 15:33:33 +0700

    Success

---
+++ app/src/main/java/appewtc/masterung/easykku/MyConstant.java
@@ -0,0 +1,15 @@
+package appewtc.masterung.easykku;
+
+/**
+ * Created by masterUNG on 11/13/2016 AD.
+ */
+
+public class MyConstant {
+
+    //Explicit
+    private String urlGetJSON = "http://swiftcodingthai.com/kku/get_user_master.php";
+
+    public String getUrlGetJSON() {
+        return urlGetJSON;
+    }
+}   // Main Class
